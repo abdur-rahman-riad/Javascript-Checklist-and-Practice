d77bcda4feff5af7ead1f25d6c820e2b3c476212
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-08-08 20:54:24 +0600

    Checklist and Practice

--- problem-16.js
+++ problem-16.js
@@ -1,10 +1,33 @@
-// Print Largets String from an Array using Function
 function bestFriend(friend) {
+    let max = 0;
+    let largestString;
     for (let i = 0; i < friend.length; i++) {
-        elements = friend[i];
-        // console.log(elements);
+        console.log(friend[i], friend[i].length);
+        if (friend[i].length > max) {
+            max = friend[i].length;
+            largestString = friend[i];
+        }
     }
-    return elements;
+    console.log("Largest String: " + largestString + " & Length is: " + max);
+    // return largestString;
+}
+friendList = ["Riad Rahman", "Mohammad Abdur Rahman", "Mohammad", "Rahman"];
+console.log(bestFriend(friendList));
+
+
+
+
+/* // Print Largets String from an Array using Function
+function bestFriend(friend) {
+    let max = 0;
+    let largestString;
+    for (let i = 0; i < friend.length; i++) {
+        if (friend[i].length > max) {
+            largestString = friend[i];
+        }
+        console.log(friend[i], friend[i].length);
+    }
+    console.log("Largets Name Have: " + largestString + " Character");
 }
 friendList = ["Riad", "Abdur", "Mohammad", "Rahman"];
-console.log(bestFriend(friendList));
+console.log(bestFriend(friendList)); */
